9aa12603ffbef477c58e00891c6048adf08cdb69
Tercio <prtercio@gmail.com>
2017-03-13 17:07:06 -0600

    6to commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -51,48 +51,123 @@ app.controller('PlaylistsCtrl', function($scope, $http) {
     { title: 'Cowbell', id: 6 }
   ];
 
-  //var url = 'https://xbl.io/api/v1/account'; //'http://hechoennl.gob.mx/services/getCategorias2';
+/*
+  var xuid;
+
+  var url = 'https://xbl.io/api/v1/account'; //'http://hechoennl.gob.mx/services/getCategorias2';
+  var url2 = 'https://jsonplaceholder.typicode.com/posts/1';
+  var url3 = 'https://xboxapi.com/v2/accountxuid';
+  var url4 = 'https://xboxapi.com/v2/xuid/BruceGrannec'
   $http({
-    url:'https://jsonplaceholder.typicode.com/posts/1',
+    url: url4,
     method: 'GET',
     headers: {
                 'Access-Control-Allow-Origin': '*',                
-                'X-Authorization': '5b3e63aa-4a0b-4dd6-a640-50c1070cfbea',
-                'Access-Control-Allow-Methods': 'GET',
-                'Content-Type':'application/json'
+                //'X-AUTH': '4a58d6c0d49e5884e43a756d729940c95c82cca7' //Benbaodan
+                'X-AUTH' : '5056c2081205740a2d765ebe3ff5807dd4178a87'
+                //'Access-Control-Allow-Methods': 'GET',
+                //'Content-Type':'application/json'
             }
   }).then(function(resp) {
-            console.log("Resp: "+resp);
-            $scope.respuesta = resp;
+            console.log("Resp: "+resp.data.xuid);
+            //$scope.respuesta = resp;
+            xuid = resp.data.xuid;
+
+            verDatos(xuid);
            
   }, function(err) {
            console.log(err.data);
-           $scope.error = err;
+           //$scope.error = err;
   });
- 
-
-
-  /*
-$http.jsonp({
-    //url:'https://xbl.io/api/v1/Benbaodan',
-    url: 'http://hechoennl.gob.mx/services/getCategorias2',
-    method: 'GET',   
-    headers: {
-                'Access-Control-Allow-Origin': '*',                
-                //'X-Authorization': '5b3e63aa-4a0b-4dd6-a640-50c1070cfbea',
-                'Access-Control-Allow-Methods': 'GET',
-                'Content-Type':'application/json'
-            }
-
-  }).then(function(resp) {
-          console.log("1 "+ resp);
-            }, function(err) {
-              $scope.error =err;
-            });
-
-  $scope.prova = "ABC";
   */
+  
+ // uid Diego: 2535463553180810;
+ // uid Bruce; 2533274954006095
+ //uid Benbaodan: 2533274906763963
+// function verDatos (xuid){
+  var xuid = 2533274906763963;
+  var url3 = 'https://xboxapi.com/v2/messages';
+  var url5 = 'https://xboxapi.com/v2/'+xuid+'/friends';
+  var url8 = 'https://xboxapi.com/v2/'+xuid+'/gamercard'
+  var url6 = 'https://xboxapi.com/v2/'+xuid+'/presence';
+  var url9 = 'https://xboxapi.com/v2/'+xuid+'/game-clips';
+  var url7 ='https://xbl.io/api/v1/account';
+  var idXbl = '946767cd-422c-472e-b001-4f4a4fdace4c';
 
+
+
+  $scope.buscrXuid = function (gamertag){
+    var url = 'https://xboxapi.com/v2/xuid/'+gamertag;
+    $http({
+      url: url,
+      method: 'GET',
+      headers: {
+                  'Access-Control-Allow-Origin': '*',                
+                  'X-AUTH': '4a58d6c0d49e5884e43a756d729940c95c82cca7', //Benbaodan               
+                  'Content-Type':'application/json'
+              }
+    }).then(function(resp) { 
+              //console.log("Resp2: "+resp.data.devices[0].titles[1].activity.richPresence);
+              console.log("Resp2: "+resp.data.xuid);
+              $scope.respuesta = resp.data;
+               //verDatos(xuid);
+             
+    }, function(err) {
+             console.log("Error2 "+err.data);
+             $scope.error = err;
+    });
+
+
+
+  }
+
+  $scope.irHttp = function(){
+    var xuid = 2533275028767498;
+     var urlJ = 'https://xboxapi.com/v2/'+xuid+'/presence';
+    $http({
+      url: urlJ,
+      method: 'GET',
+      headers: {
+                  'Access-Control-Allow-Origin': '*',                
+                  'X-AUTH': '4a58d6c0d49e5884e43a756d729940c95c82cca7', //Benbaodan
+                  //'X-AUTH' : '5056c2081205740a2d765ebe3ff5807dd4178a87' // BenbaodanJr
+                  //'X-Authorization':idXbl,
+                  //'Access-Control-Allow-Methods': 'GET',
+                  'Content-Type':'application/json'
+              }
+    }).then(function(resp) {
+              //console.log("Resp2: "+resp.data.devices[0].titles[1].activity.richPresence);             
+              $scope.respuesta = resp.data;
+
+
+              //verDatos(xuid);
+             
+    }, function(err) {
+             console.log("Error2 "+err.data);
+             $scope.error = err;
+    });
+ }
+
+ var urlLocal = 'js/BD/presenceJonathan.json';
+
+ $http.get(urlLocal).then(function(resp) {
+              console.log("Resp2: "+resp.data); 
+              console.log("Resp2: "+resp.data.devices[0].titles[0].activity.richPresence);               
+              $scope.respuesta = resp.data;
+              var resultado = resp.data.devices[0].titles[0].activity.richPresence;
+              var separador = ""; // un espacio en blanco
+              var limite    = 1;
+              var resposta = resultado.split(separador);
+              console.log(resposta[27], resposta[29])       
+
+
+  }, function(err) {
+             console.log("Error2 "+err.data);
+             $scope.error = err;
+  });
+
+
+  
 
 });
 
